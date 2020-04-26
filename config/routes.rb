0eb6da2cc22087c735774fd2f10d774ae3587504
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-26 08:16:07 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'artists/create'
+
+  get 'artists/update'
+
+  get 'artists/show'
+
+  get 'genres/create'
+
+  get 'genres/update'
+
+  get 'genres/show'
+
+  get 'song/create'
+
+  get 'song/update'
+
+  get 'song/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
