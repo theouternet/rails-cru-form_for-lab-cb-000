b5e8c7891859d3203be646ae54d659436e554ce3
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-26 19:26:47 +0000

    Automatically backed up by Learn

--- spec/requests/artists_request_spec.rb
+++ spec/requests/artists_request_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Artists", type: :request do
 
- # describe "GET /create" do
+ describe "GET /create" do
     it "returns http success" do
       get "/artists/create"
       expect(response).to have_http_status(:success)
     end
   end
 
- # describe "GET /update" do
+  describe "GET /update" do
     it "returns http success" do
       get "/artists/update"
       expect(response).to have_http_status(:success)
     end
   end
 
-#  describe "GET /show" do
+  describe "GET /show" do
     it "returns http success" do
       get "/artists/show"
       expect(response).to have_http_status(:success)
