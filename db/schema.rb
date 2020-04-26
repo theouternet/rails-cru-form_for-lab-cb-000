f4bce215da6d17f5ef747edaca10a4ac89acb62e
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-26 19:24:56 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200426072906) do
+ActiveRecord::Schema.define(version: 20200204164843) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -34,5 +34,3 @@ ActiveRecord::Schema.define(version: 20200426072906) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["genre_id"], name: "index_songs_on_genre_id"
   end
-
-end
